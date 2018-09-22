3bb075006a616762c1b9aed5766997261829a8e7
Carlos Dominguez <dominguerilla@gmail.com>
2018-09-22 18:27:31 -0400

    jumping works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,15 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 10.0f;
     public float jumpSpeed = 8.0f;
+    public float jumpHeight = 10.0f;
     public float gravity = 9.8f;
 
+
     CharacterController cha;
+    bool isJumping = false;
+    float currentHeight = 0f;
+    float increment = 0f;
+    float vertSpeed = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,18 +23,42 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 moveDirection = CalculateXY();
+        moveDirection.y = CalculateVertical();
+
+        cha.Move(moveDirection *= Time.deltaTime);
+    }
+    
+    Vector3 CalculateXY(){
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= speed * Time.deltaTime;
+        moveDirection *= speed ;
+        return moveDirection;
+    }
 
-        float vSpeed = -1.0f;
+    float CalculateVertical(){
         if(cha.isGrounded) {
-            if(Input.GetButtonDown("Jump")) {
-                vSpeed = jumpSpeed;
+            vertSpeed = 0f;
+            if (Input.GetButtonDown("Jump")) {
+                Debug.Log("JUMP");
+                isJumping = true;
             }
         }
-        vSpeed -= gravity * Time.deltaTime;
-        moveDirection.y = vSpeed;
-        cha.Move(moveDirection);
+
+        // gradually ascend
+        if (isJumping){
+            increment += jumpSpeed * Time.deltaTime;
+            currentHeight = Mathf.Lerp(0, jumpHeight, increment);
+            if(currentHeight >= jumpHeight){
+                currentHeight = 0f;
+                isJumping = false;
+                increment = 0f;
+            }else{
+                vertSpeed = currentHeight;
+            }
+        }else{
+            vertSpeed -= gravity * Time.deltaTime;
+        }
+        return vertSpeed;
     }
 }
